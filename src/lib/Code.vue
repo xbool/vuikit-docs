89e699ee282f9ea893bb08e5f21cea651851d444
Miljan Aleksic <miljan@aleksic.es>
2016-06-26 10:10:53 +0700

    update vk-docs-code
    add code prop
    improve workflow

--- src/lib/Code.vue
+++ src/lib/Code.vue
@@ -1,39 +1,38 @@
 <template>
-  <pre></pre>
+  <pre v-html="code | beautify | highlight"></pre>
 </template>
 
 <script>
 import beautify from 'js-beautify'
 import hljs from 'highlight.js/lib/highlight'
-// init html highlighting
-hljs.registerLanguage('html', require('highlight.js/lib/languages/xml'))
 
 export default {
-  compiled () {
-    // retrieve content from default slot
-    const div = document.createElement('div')
-    const fragment = this._slotContents.default
-    div.appendChild(fragment.cloneNode(true))
-    let text = div.innerHTML.trim()
-    // apply effects and set on code
-    text = this.beautify(text)
-    text = this.highlight(text)
-    this.$el.innerHTML = text
+  init () {
+    // init html highlighting
+    hljs.registerLanguage('html', require('highlight.js/lib/languages/xml'))
   },
-  methods: {
-    update (value) {
-      value = this.beautify(value)
-      value = this.highlight(value)
-      this.el.innerHTML = value
-    },
+  props: {
+    code: {
+      type: String,
+      default: ''
+    }
+  },
+  compiled () {
+    if (!this.code && this._slotContents) {
+      // if no code provided retrieve it from default slot
+      const div = document.createElement('div')
+      const fragment = this._slotContents.default
+      div.appendChild(fragment.cloneNode(true))
+      this.code = div.innerHTML.trim()
+    }
+  },
+  filters: {
     beautify (str) {
       return beautify.html(str, {
         wrap_attributes: 'force',
         wrap_attributes_indent_size: 2,
         indent_size: 2
       })
-      // add new line for closing tags, if no children
-      .replace(/><\/vk-(.*)>$/g, '>\n</vk-$1>')
     },
     highlight (str) {
       return hljs.highlight('html', str).value
